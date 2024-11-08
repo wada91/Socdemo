2bc1694dbe88cb2e01e5058fe4a5da99db7318c2
wada91 <76800432+wada91@users.noreply.github.com>
2024-11-08 11:12:03 +0100

    Add files via upload

--- src/pages/Airdrop.tsx
+++ src/pages/Airdrop.tsx
@@ -1,8 +1,51 @@
-// Airdrop.js
+// Airdrop.tsx
 import React from 'react';
+import './Airdrop.css';
+import Awright from './icons/Awright';
 
-const Airdrop: React.FC = () => {
-  return <div className="text-center text-white text-2xl">Aridrop Page</div>;
+const Earn: React.FC = () => {
+  return (
+    <div className="airdrop-page">
+      {/* Top Icon */}
+      <div className="airdrop-icon-container">
+        
+      </div>
+      
+      
+
+      {/* Title */}
+      <h1 className="airdrop-title">Airdrop
+      <p className='copy-text'>Conect your wallet to get the Airdrop
+        
+        </p>
+      </h1>
+     
+
+      {/* SocialCoin Youtube Section */}
+      <section className="airdrop-section">
+        
+        
+        {/* Task Card */}
+        <div className="task-card">
+          <div className="task-card-bg"></div> {/* Background layer for blur effect */}
+          <div className="task-icon wallet-icon" />
+          <div className="task-content">
+            <p className="task-title">Telegram wallet</p>
+            <p className="task-reward">
+              Connect your Wallet to get your airdrop
+            </p>
+            
+          </div>
+          <div className="aw-icon">
+          <Awright size={32} /> 
+          </div>
+        </div>
+        
+       
+      </section>
+     
+    </div>
+  );
 };
 
-export default Airdrop;
+export default Earn;
